0f31977aabdd4a851d11fec90a58ea5c61bd7e9f
Whispersmith <78102201+Whispersmith@users.noreply.github.com>
2023-10-14 00:03:52 +0300

    lib

--- src/test/java/lib/Platform.java
+++ src/test/java/lib/Platform.java
@@ -59,12 +59,12 @@ public class Platform {
         return capabilities;
     }
     private ChromeOptions getMyChromeOptions(){
-        Map<String, Object> deviceMetrics = new HashMap<>();
+        Map<String, Object> deviceMetrics = new HashMap<String, Object>();
         deviceMetrics.put("width", 360);
         deviceMetrics.put("height", 640);
         deviceMetrics.put("pixelRatio", 3.0);
 
-        Map<String, Object> mobileEmulation = new HashMap<>();
+        Map<String, Object> mobileEmulation = new HashMap<String, Object>();
         mobileEmulation.put("deviceMetrics", deviceMetrics);
         mobileEmulation.put("userAgent", "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D)AppleWebKit/535.19 (KHTML, like Gecko)Chrome/18.0.1025.166 Mobile Safari/535.19");
 
